72758b92ee40e4b6787fd4839512b4669d5c1781
germankuchen <germankuchen@gmail.com>
2018-08-20 11:30:49 -0300

    Mejoras de carousel

--- fedekuchen/src/main/webapp/js/controllers/mainCtrl.js
+++ fedekuchen/src/main/webapp/js/controllers/mainCtrl.js
@@ -2,10 +2,19 @@ angular.module('myApp').controller("MainCtrl",
 function($scope, $location) {
 	console.log('Llego al Main');
 	
+	$scope.init = function()
+	{
+		$('.carousel').carousel({
+	        interval: 4000
+	    })
+	}
+	
 	$scope.irADisenio = function()
 	{
 		$location.path('/disenio');
 	}
+	
+	$scope.init();
 	
 });
 
